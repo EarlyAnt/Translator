e82d9913db06c5069b9f5d5a77a0bbcc608e6384
EarlyAnt <54763755@qq.com>
2022-06-22 18:03:50 +0800

    1.将translate_one方法和traslate_multi方法改成带参数的形式。

--- Translator/src/py/translator.py
+++ Translator/src/py/translator.py
@@ -1,28 +1,29 @@
 from googletrans import Translator
 
+
 def test():
     import googletrans
-    print (googletrans.LANGUAGES)
+    print(googletrans.LANGUAGES)
 
-def translate_one():
+
+def translate_one(content, src, dest):
     translator = Translator()
-    result = translator.translate('相信一切都是最好的安排！', src='zh-cn', dest='en')
+    result = translator.translate(text=content, src=src, dest=dest)
 
     print(result.src)
     print(result.dest)
     print(result.text)
 
-def translate_multi():
-    words = ['健康', '平安', '幸福', '快乐', '如意']
-    
+
+def translate_multi(words, src, dest):
     translator = Translator()
-    results = translator.translate(words, src='zh-cn', dest='en')
+    results = translator.translate(words, src=src, dest=dest)
     for result in results:
         print("{}->{}".format(result.origin, result.text))
 
 
 if __name__ == "__main__":
     #  test()
-    #  translate_one()
-     translate_multi()
-    
+    # translate_one(content="相信一切都是最好的安排！", src="zh-cn", dest="en")
+    translate_multi(words=['健康', '平安', '幸福', '快乐', '如意'],
+                    src='zh-cn', dest='en')
