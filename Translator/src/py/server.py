ffdb29f073e263c82ba3686902e7ae541df520b4
EarlyAnt <54763755@qq.com>
2022-06-22 15:48:13 +0800

    1.新增run_server.bat脚本，方便启动服务器脚本。 2.新增index.html测试页面，仅用于访问flask服务器时，返回默认页面。 3.server.py脚本，增加允许跨域访问的设置。 4.index2.html脚本，实现了post提交json类型的数据，并从服务器获取json格式的返回值。

--- Translator/src/py/server.py
+++ Translator/src/py/server.py
@@ -1,35 +1,53 @@
-from flask import Flask,jsonify,render_template
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 # from gevent import pywsgi
 import json
- 
-app = Flask(__name__)#实例化app对象
- 
-testInfo = {}
- 
-@app.route('/translate_one',methods=['GET','POST'])#路由
-def test_post():
-    # testInfo['name'] = 'xiaoming'
-    # testInfo['age'] = '28'
-    # return json.dumps(testInfo)
 
-    json_data = request.json
-    logger.info(f"json_data:{json_data}")
-    return jsonify(json_data)
- 
+app = Flask(__name__)  # 实例化app对象
+# CORS(app, supports_credentials=True)
+CORS(app)
+
+testInfo = {}
+
+
+@app.route('/translate_one', methods=['GET', 'POST'])  # 路由
+def translate_one():
+    if request.method == 'POST':
+        # testInfo['name'] = '毛晓彤'
+        # testInfo['age'] = '28'
+        # return json.dumps(testInfo)
+
+        print("reqeust: {}".format(request.json))
+        json_data = request.json
+        print("translate_one->data: {}".format(json_data))
+        return jsonify(json_data)
+    else:
+        testInfo['name'] = '白冰'
+        testInfo['age'] = '29'
+        return json.dumps(testInfo)
+
+        # print(request.json)
+        # return render_template('index.html')
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
- 
+def default():
+    # return 'Hello World!'
+    testInfo['name'] = 'xiaoming'
+    testInfo['age'] = '28'
+    return json.dumps(testInfo)
+
+
 @app.route('/index')
 def index():
     return render_template('index.html')
- 
- 
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',#任何ip都可以访问
-            port=5550,#端口
+    app.run(host='0.0.0.0',  # 任何ip都可以访问
+            port=5550,  # 端口
             debug=True
             )
-    
+
     # server = pywsgi.WSGIServer(('0.0.0.0', 5550), app)
     # server.serve_forever()
