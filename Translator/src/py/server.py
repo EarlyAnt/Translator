ea484841a4821f1a854c3de8c43f15df58f0b885
EarlyAnt <54763755@qq.com>
2022-06-22 21:23:48 +0800

    1.translator.py脚本，translate_one方法和translate_multi方法都加上了返回值。 2.server.py脚本，调用translator.py脚本中的方法，实现了翻译request提交的文字的功能。 3.index2.html脚本，页面上放2个按钮，一个访问translate_one接口，一个访问translate_multi接口。

--- Translator/src/py/server.py
+++ Translator/src/py/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+import translator
 # from gevent import pywsgi
 import json
 
@@ -20,14 +21,36 @@ def translate_one():
         print("reqeust: {}".format(request.json))
         json_data = request.json
         print("translate_one->data: {}".format(json_data))
-        return jsonify(json_data)
-    else:
-        testInfo['name'] = '白冰'
-        testInfo['age'] = '29'
-        return json.dumps(testInfo)
+
+        data = translator.translate_one(
+            content=json_data['content'], src=json_data['src'], dest=json_data['dest'])
+        print("json_data: {}".format(data))
+        return jsonify(data)
+
+    # else:
+    #     testInfo['name'] = '白冰'
+    #     testInfo['age'] = '29'
+    #     return json.dumps(testInfo)
 
         # print(request.json)
         # return render_template('index.html')
+
+
+@app.route('/translate_multi', methods=['GET', 'POST'])  # 路由
+def translate_multi():
+    if request.method == 'POST':
+        print("reqeust: {}".format(request.json))
+        json_data = request.json
+        print("translate_multi->data: {}".format(json_data))
+
+        data = translator.translate_multi(
+            words=json_data['content'], src=json_data['src'], dest=json_data['dest'])
+        print("json_data: {}".format(data))
+        return jsonify(data)
+    # else:
+    #     testInfo['name'] = '白冰'
+    #     testInfo['age'] = '29'
+    #     return json.dumps(testInfo)
 
 
 @app.route('/')
@@ -44,7 +67,9 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',  # 任何ip都可以访问
+    app.run(host='192.168.0.102',
+            # host='192.168.180.142',
+            # host='0.0.0.0',  # 任何ip都可以访问
             port=5550,  # 端口
             debug=True
             )
--- Translator/src/py/translator.py
+++ Translator/src/py/translator.py
@@ -14,16 +14,25 @@ def translate_one(content, src, dest):
     print(result.dest)
     print(result.text)
 
+    return {content: result.text}
+
 
 def translate_multi(words, src, dest):
+    translated = {}
+
     translator = Translator()
     results = translator.translate(words, src=src, dest=dest)
+
     for result in results:
         print("{}->{}".format(result.origin, result.text))
+        translated[result.origin] = result.text
+    return translated
 
 
 if __name__ == "__main__":
     #  test()
-    # translate_one(content="相信一切都是最好的安排！", src="zh-cn", dest="en")
-    translate_multi(words=['健康', '平安', '幸福', '快乐', '如意'],
-                    src='zh-cn', dest='en')
+
+    translate_one(content="相信一切都是最好的安排！", src="zh-cn", dest="en")
+
+    # words = ['健康', '平安', '幸福', '快乐', '如意']
+    # translate_multi(words=words, src='zh-cn', dest='en')
